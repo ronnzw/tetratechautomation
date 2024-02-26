989b53507294ae3f744e8b2628313b79f816b36a
ronnzw <rmaravanyika@gmail.com>
2024-02-26 22:54:24 +0200

    about page

--- tetautoapp/urls.py
+++ tetautoapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('ourServices',views.ourServices,name="ourServices"),
 ]
--- tetautoapp/views.py
+++ tetautoapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,'tetautoapp/home.html',{})
 
+def about(request):
+    return render(request,'tetautoapp/about.html',{})
+
+def ourServices(request):
+    return render(request,'tetautoapp/OurServices.html',{})
 # Create your views here.
